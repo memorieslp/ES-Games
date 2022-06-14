90f4c2aecd6f36c30529c157fec7d35c1e22bf6a
MemoriesLP <raphaelrangel2005@yahoo.com.br>
2022-06-14 14:44:34 -0300

    Testing game highNumber

--- tests.py
+++ tests.py
@@ -1,12 +1,22 @@
 import unittest
 
-from games import VICTORY, higherNumber
+from games import DEFEAT, DRAW, VICTORY, higherNumber
 
 class TestGames(unittest.TestCase):       
-    def test_highNumber(self):
+    def test_highNumber_playerWin(self):
         number1 = 10
         number2 = 5
         self.assertEqual(higherNumber(number1, number2), VICTORY)
+    
+    def test_highNumber_playerLose(self):
+        number1 = 5
+        number2 = 10
+        self.assertEqual(higherNumber(number1, number2), DEFEAT)
+        
+    def test_highNumber_Draw(self):
+        number1 = 5
+        number2 = 5
+        self.assertEqual(higherNumber(number1, number2), DRAW)
 
 if __name__ == "__main__":
     unittest.main()
