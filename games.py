a08e1d3e2bc6e472f0715485ab263b2a29dbdd5e
MemoriesLP <raphaelrangel2005@yahoo.com.br>
2022-06-14 15:01:54 -0300

    Unit Tests for compareResults and predictNumber. compareResults was created to avoid duplication

--- games.py
+++ games.py
@@ -4,13 +4,21 @@ VICTORY = 2
 DEFEAT = 1
 DRAW = 0
 
-def higherNumber(number1, number2):
-    if number1 > number2:
+def compareResults(result1, result2):
+    if result1 > result2:
         return VICTORY
-    elif number1 < number2:
+    elif result1 < result2:
         return DEFEAT
     else:
         return DRAW
+
+def higherNumber(number1, number2):
+    return compareResults(number1, number2)
+
+def predictNumber(number1, number2, number_to_predict):
+    result1 = abs(number1 - number_to_predict)
+    resunt2 = abs(number2 - number_to_predict)
+    return compareResults(resunt2, result1) 
 
 def main():
     game_to_play = int(input())
--- tests.py
+++ tests.py
@@ -1,8 +1,25 @@
 import unittest
 
-from games import DEFEAT, DRAW, VICTORY, higherNumber
+from games import DEFEAT, DRAW, VICTORY, compareResults, higherNumber, predictNumber
 
-class TestGames(unittest.TestCase):       
+class TestGames(unittest.TestCase):
+    #TEST COMPARE RESULTS
+    def test_compareResults_playerWin(self):
+        result1 = 10
+        result2 = 5
+        self.assertEqual(compareResults(result1, result2), VICTORY)
+    
+    def test_compareResults_playerLose(self):
+        result1 = 5
+        result2 = 10
+        self.assertEqual(compareResults(result1, result2), DEFEAT)
+        
+    def test_compareResults_Draw(self):
+        result1 = 5
+        result2 = 5
+        self.assertEqual(compareResults(result1, result2), DRAW)
+    
+    #TEST HIGH NUMBER            
     def test_highNumber_playerWin(self):
         number1 = 10
         number2 = 5
@@ -18,5 +35,24 @@ class TestGames(unittest.TestCase):
         number2 = 5
         self.assertEqual(higherNumber(number1, number2), DRAW)
 
+    #TEST PREDICT NUMBER 
+    def test_predictNumber_playerWin(self):
+        number1 = 10
+        number2 = 5
+        number_to_predict = 9
+        self.assertEqual(predictNumber(number1, number2, number_to_predict), VICTORY)
+    
+    def test_predictNumber_playerLose(self):
+        number1 = 10
+        number2 = 5
+        number_to_predict = 6
+        self.assertEqual(predictNumber(number1, number2, number_to_predict), DEFEAT)
+        
+    def test_predictNumber_Draw(self):
+        number1 = 6
+        number2 = 4
+        number_to_predict = 5
+        self.assertEqual(predictNumber(number1, number2, number_to_predict), DRAW)
+        
 if __name__ == "__main__":
     unittest.main()
